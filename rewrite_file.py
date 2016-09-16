7c8db1f80da2d2db05724ca4153d314bd194ab35
Matthew D Jones <matthew.d.jones@tessella.com>
2016-09-16 16:56:55 +0100

    start implementing copy functions which allow compression type change

--- rewrite_file.py
+++ rewrite_file.py
@@ -23,6 +23,30 @@ def copy_group_with_attributes(out_file, in_file, group_name):
             out_file[group_name].__delitem__(sub_group)
 
 
+def copy_dataset_with_attributes(out_file, in_file, source_dataset, compress_type='gzip',
+                                 compress_opts=1, target_dataset=None):
+    if not target_dataset:
+        target_dataset = source_dataset
+    data = in_file.get(source_dataset)
+    target_data = out_file.create_dataset(target_dataset, data[...].shape, dtype=data.dtype,
+                                          compression=compress_type, compression_opts=compress_opts)
+    target_data[...] = data[...]
+    target_data.attrs = data.attrs
+
+
+def copy_object(name):
+    pass
+    # do copy_group_with_attributes if it is a group
+    # do copy_dataset_with_attributes if it is a dataset
+
+
+def copy_all(out_file, in_file, group_name, compress_type='gzip', compress_opts=1):
+    in_group = in_file[group_name]
+    group = out_file.create_group(group_name)
+    group.attrs = in_file[group_name].attrs
+    in_group.visit(copy_object)
+
+
 def create_reduced_file_for_comparison(source_filename, target_filename, datasets):
     """
     Create a reduced version of the original file to compare in size to the ESS format file
@@ -35,6 +59,8 @@ def create_reduced_file_for_comparison(source_filename, target_filename, dataset
         with h5py.File(target_filename, 'r+') as target_file:
             copy_group_with_attributes(target_file, source_file, '/raw_data_1')
             copy_group_with_attributes(target_file, source_file, '/raw_data_1/detector_1_events')
+            copy_group_with_attributes(target_file, source_file, '/raw_data_1/instrument')
+            target_file.copy(source_file['/raw_data_1/instrument/source'], '/raw_data_1/instrument/source')
             for dataset in datasets:
                 target_file.copy(source_file[dataset], dataset)
 
@@ -55,6 +81,8 @@ def rewrite_to_ess_format(source_filename, target_filename, compress_type='gzip'
             instr_group = entry_group.create_group('instrument')
             instr_group.attrs['NX_class'] = 'NXinstrument'
 
+            target_file.copy(source_file['/raw_data_1/instrument/source'], '/raw_data_1/instrument/source')
+
             det_group = instr_group.create_group('detector_1')
             det_group.attrs['NX_class'] = 'NXdetector'
 
@@ -69,14 +97,14 @@ def rewrite_to_ess_format(source_filename, target_filename, compress_type='gzip'
             data_time = source_file.get('/raw_data_1/detector_1_events/event_time_offset')
             event_time = event_group.create_dataset('time', data_time[...].shape, dtype=np.dtype('u4'),
                                                     compression=compress_type, compression_opts=compress_opts)
-            event_time[...] = data_time[...]*1000
+            event_time[...] = data_time[...] * 1000
             event_time.attrs['relative_to'] = 'step'
             event_time.attrs['tick_length'] = 1  # nanoseconds
 
             pulse_time = source_file.get('/raw_data_1/detector_1_events/event_time_zero')
             step_time = event_group.create_dataset('step_time', pulse_time[...].shape, dtype=np.dtype('u4'),
                                                    compression=compress_type, compression_opts=compress_opts)
-            step_time[...] = pulse_time[...]*100
+            step_time[...] = pulse_time[...] * 100
             step_time.attrs['tick_length'] = 10000000  # nanoseconds (10 milliseconds)
 
             event_index = source_file.get('/raw_data_1/detector_1_events/event_index')
